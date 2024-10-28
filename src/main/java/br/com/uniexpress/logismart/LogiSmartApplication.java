03b7d252f9dd1f3117c768e3cf4c29cc6c54f1dd
Breno Daros <brenosouzadaros@gmail.com>
2024-10-28 12:23:13 -0300

    initial commit

---
+++ src/main/java/br/com/uniexpress/logismart/LogiSmartApplication.java
@@ -0,0 +1,13 @@
+package br.com.uniexpress.logismart;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LogiSmartApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LogiSmartApplication.class, args);
+	}
+
+}
