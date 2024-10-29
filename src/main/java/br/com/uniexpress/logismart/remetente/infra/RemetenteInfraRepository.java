b6c0dae91b80ac5feb50fb7a50ab93641bdae65c
Breno Daros <brenosouzadaros@gmail.com>
2024-10-29 12:35:37 -0300

    implementa repository e ajusta configuracoes do hibernate no application.yml

--- src/main/java/br/com/uniexpress/logismart/remetente/infra/RemetenteInfraRepository.java
+++ src/main/java/br/com/uniexpress/logismart/remetente/infra/RemetenteInfraRepository.java
@@ -9,9 +9,13 @@ import org.springframework.stereotype.Repository;
 @Repository
 @RequiredArgsConstructor
 public class RemetenteInfraRepository implements RemetenteRepository {
+    private final RemetenteSpringDataJPARepository remetenteSpringDataJPARepository;
+
     @Override
     public void salvaRemetente(Remetente remetente) {
         log.debug("[start] RemetenteInfraRepository - salvaRemetente");
+        remetenteSpringDataJPARepository.save(remetente);
+        // TODO Tratar exeption caso campos únicos sejam duplicados
         log.debug("[finish] RemetenteInfraRepository - salvaRemetente");
     }
 }
