46dcb97c114bf88233cf3aff77d2a5d79b1869db
Breno Daros <brenosouzadaros@gmail.com>
2024-10-30 10:34:46 -0300

    implementa contrato de api

--- src/main/java/br/com/uniexpress/logismart/remetente/application/api/RemetenteController.java
+++ src/main/java/br/com/uniexpress/logismart/remetente/application/api/RemetenteController.java
@@ -35,4 +35,10 @@ public class RemetenteController implements RemetenteAPI {
         remetenteService.alteraRemetente(alteraRemetenteRequest, idRemetente);
         log.debug("[finish] RemetenteController - patchAlteraRemetente");
     }
+
+    @Override
+    public void deleteDeletaRemetente(UUID idRemetente) {
+        log.debug("[start] RemetenteController - deleteDeletaRemetente");
+        log.debug("[finish] RemetenteController - deleteDeletaRemetente");
+    }
 }
