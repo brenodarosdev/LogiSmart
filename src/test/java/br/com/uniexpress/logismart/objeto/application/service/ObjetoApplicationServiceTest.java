e915269cfa01e633310946d5e7a4d9c11f54cd17
Breno Daros <brenosouzadaros@gmail.com>
2024-10-30 21:39:25 -0300

    implementa testes unitarios do cenario deveSalvarNovoObjeto

---
+++ src/test/java/br/com/uniexpress/logismart/objeto/application/service/ObjetoApplicationServiceTest.java
@@ -0,0 +1,61 @@
+package br.com.uniexpress.logismart.objeto.application.service;
+
+import br.com.uniexpress.logismart.destinatario.domain.Destinatario;
+import br.com.uniexpress.logismart.destinatario.infra.DestinatarioRepository;
+import br.com.uniexpress.logismart.objeto.application.api.NovoObjetoResponse;
+import br.com.uniexpress.logismart.objeto.application.api.ObjetoRequest;
+import br.com.uniexpress.logismart.objeto.domain.Objeto;
+import br.com.uniexpress.logismart.objeto.infra.ObjetoRepository;
+import br.com.uniexpress.logismart.remetente.domain.Remetente;
+import br.com.uniexpress.logismart.remetente.infra.RemetenteRepository;
+import br.com.uniexpress.logismart.util.DataHelper;
+import br.com.uniexpress.logismart.viacep.application.service.ViaCepResponse;
+import br.com.uniexpress.logismart.viacep.application.service.ViaCepService;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
+
+import java.util.UUID;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.when;
+
+@ExtendWith(MockitoExtension.class)
+class ObjetoApplicationServiceTest {
+    @InjectMocks
+    private ObjetoApplicationService objetoApplicationService;
+    @Mock
+    private RemetenteRepository remetenteRepository;
+    @Mock
+    private DestinatarioRepository destinatarioRepository;
+    @Mock
+    private ViaCepService viaCepService;
+    @Mock
+    private ObjetoRepository objetoRepository;
+
+    @Test
+    void deveSalvarNovoObjeto() {
+        Remetente remetente = DataHelper.criaRemetente();
+        UUID idRemetente = remetente.getId();
+        Destinatario destinatario = DataHelper.criaDestinatario();
+        UUID idDestinatario = destinatario.getId();
+        ViaCepResponse enderecoRemetente = DataHelper.criaViaCepResponse();
+        ViaCepResponse enderecoDestinatario = DataHelper.criaViaCepResponse2();
+        ObjetoRequest objetoRequest = DataHelper.criaObjetoRequest();
+
+        when(remetenteRepository.buscaRemetentePorId(idRemetente)).thenReturn(remetente);
+        when(destinatarioRepository.buscaDestinatarioPorId(idDestinatario)).thenReturn(destinatario);
+        when(viaCepService.consultaCep(remetente.getCep())).thenReturn(enderecoRemetente);
+        when(viaCepService.consultaCep(destinatario.getCep())).thenReturn(enderecoDestinatario);
+        NovoObjetoResponse novoObjetoResponse = objetoApplicationService.novoObjeto(objetoRequest);
+
+        verify(objetoRepository).salvaObjeto(any(Objeto.class));
+        assertNotNull(novoObjetoResponse);
+        assertEquals(NovoObjetoResponse.class, novoObjetoResponse.getClass());
+    }
+}
